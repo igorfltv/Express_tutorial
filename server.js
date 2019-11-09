5daf2d60c6801ab09cee2d01daceab43ea0f1c23
igorfltv <38211024+igorfltv@users.noreply.github.com>
2019-11-09 07:17:55 +0300

    Move routes

--- server.js
+++ server.js
@@ -1,39 +1,11 @@
 const express = require("express");
 const dotenv = require("dotenv");
+const bootcamps = require("./routes/bootcamps");
+const PORT = process.env.PORT || 5000;
+const app = express();
 //
 dotenv.config({ path: "./config/config.env" });
-const app = express();
-const PORT = process.env.PORT || 5000;
-//
-
-bootcamps = ["First", "Send", "Third"];
-
-
-app.get("/api/v1/bootcamps", (req, res) => {
-  res.status(200);
-  res.send({ success: true, msg: "List Bootcamps" });
-});
-
-app.get("/api/v1/bootcamps/:id", (req, res) => {
-  res.status(200);
-  res.send({ success: true, msg: "Get Bootcamp" });
-});
-
-app.post("/api/v1/bootcamps", (req, res) => {
-  res.status(201);
-  res.send({ success: true, msg: "Create new Bootcamp" });
-});
-
-app.put("/api/v1/bootcamps/:id", (req, res) => {
-  res.status(200);
-  res.send({ success: true, msg: `Update Bootcamp ${req.params.id}` });
-});
-
-app.delete("/api/v1/bootcamps/:id", (req, res) => {
-  res.status(200);
-  res.send({ success: true, msg: `Delete Bootcamp ${req.params.id}` });
-});
-
+app.use("/api/v1/bootcamps",bootcamps);
 //
 app.listen(PORT, () => {
   console.log(
