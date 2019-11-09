efb1a8a3dc3a3a97ba98661a0215e13b2b208637
igorfltv <38211024+igorfltv@users.noreply.github.com>
2019-11-09 22:50:30 +0300

    Logger

--- server.js
+++ server.js
@@ -1,12 +1,17 @@
 const express = require("express");
 const dotenv = require("dotenv");
 const bootcamps = require("./routes/bootcamps");
-const PORT = process.env.PORT || 5000;
+const morgan = require("morgan");
 const app = express();
 //
 dotenv.config({ path: "./config/config.env" });
-app.use("/api/v1/bootcamps",bootcamps);
 //
+if (process.env.NODE_ENV === "development") {
+  app.use(morgan('dev'));
+}
+app.use(process.env.URL, bootcamps);
+//
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(
     `Server is running on ${PORT} using ${process.env.NODE_ENV} enviroment`
