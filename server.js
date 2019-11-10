a138a4b5f0217ae3fee16b65671120a08cb677f1
igorfltv <38211024+igorfltv@users.noreply.github.com>
2019-11-10 07:57:41 +0300

    DB

--- server.js
+++ server.js
@@ -1,19 +1,28 @@
 const express = require("express");
 const dotenv = require("dotenv");
-const bootcamps = require("./routes/bootcamps");
 const morgan = require("morgan");
-const app = express();
-//
+const bootcamps = require("./routes/bootcamps");
+const connectDB = require("./config/db");
+
 dotenv.config({ path: "./config/config.env" });
 //
+connectDB();
+const app = express();
+
 if (process.env.NODE_ENV === "development") {
-  app.use(morgan('dev'));
+  app.use(morgan("dev"));
 }
 app.use(process.env.URL, bootcamps);
-//
+
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
+const server = app.listen(PORT, () => {
   console.log(
     `Server is running on ${PORT} using ${process.env.NODE_ENV} enviroment`
   );
+}); 
+
+// Exception handle
+process.on("unhandledRejection", (err, promise) => {
+  console.log(`Error ${err.message}`);
+  server.close(() => process.exit(1));
 });
