94d478a5b160d4ee930641a305b9a6c8468b785f
igorfltv <38211024+igorfltv@users.noreply.github.com>
2019-11-16 13:29:08 +0300

    register

--- controllers/auth.js
+++ controllers/auth.js
@@ -6,6 +6,17 @@ const asyncHandler = require("../middleware/async");
 // @route   POST /api/v1/auth/register
 // @access  Public
 exports.registerUser = asyncHandler(async (req, res, next) => {
+  const { name, email, password, role } = req.body;
+
+  //   Create user
+
+  const user = await User.create({
+    name,
+    email,
+    password,
+    role
+  });
+
   res.status(200).json({
     success: true
   });
