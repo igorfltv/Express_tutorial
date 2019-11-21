e3b35057d47b7ae4f0f2d5ca6f6716621394e0ed
igorfltv <38211024+igorfltv@users.noreply.github.com>
2019-11-21 08:50:33 +0300

    Limit + Hpp

--- server.js
+++ server.js
@@ -5,6 +5,8 @@ const path = require("path");
 const mongoSanitize = require("express-mongo-sanitize");
 const helmet = require("helmet");
 const xssClean = require("xss-clean");
+const rateLimit = require("express-rate-limit");
+const hpp = require("hpp");
 const cookieParser = require("cookie-parser");
 const fileUpload = require("express-fileupload");
 
@@ -31,6 +33,13 @@ app.use(fileUpload());
 app.use(mongoSanitize());
 app.use(helmet());
 app.use(xssClean());
+app.use(
+  rateLimit({
+    windowsMS: process.env.WINDOWMS,
+    max: process.env.MAX_REQUEST
+  })
+);
+app.use(hpp());
 //
 app.use(express.static(path.join(__dirname, "public")));
 app.use(process.env.URL_BOOTCAMPS, bootcamps);
